17c1c77c55ba1a3a850b4283d329771de153fffc
andrelaurent <andre.laurent17@gmail.com>
2023-05-17 18:26:48 +0700

    delete select

--- src/components/CreateProspect.js
+++ src/components/CreateProspect.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import api from '../api/posts';
-import Select from 'react-select';
 
 const CreateProspect = () => {
     const [name, setName] = useState('');
