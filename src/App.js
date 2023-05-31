7e00c8a608b770d4703bb65474796839d91eb2be
devongasselyno <devon.gasselyno@soluix.ai>
2023-05-31 20:27:32 +0700

    completed client contact form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import DashboardView from './components/DashboardView';
 import { Outlet } from 'react-router-dom';
