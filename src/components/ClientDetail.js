c64f2bd5c7bbdfddabb93893c9b801d24d28cfbe
andrelaurent <andre.laurent17@gmail.com>
2023-06-07 21:55:48 +0700

    select contact/add new

--- src/components/ClientDetail.js
+++ src/components/ClientDetail.js
@@ -10,15 +10,19 @@ import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 
 const ClientDetail = () => {
     const { id } = useParams()
-    const [client, setClient] = useState({})
+    const [contact, setClient] = useState({})
     const [showAddContact, setShowAddContact] = useState(false)
     const [contactData, setContactData] = useState({
         contact_name: "",
         contact_alias: ""
     })
+    const [contacts, setContacts] = useState([])
+    const [fetchedContact, setFetchedContact] = useState(0)
     const [contactError, setContactError] = useState('')
     const [contactSnackBar, setContactSnackbar] = useState('')
     const [successSnackbar, setSuccessSnackbar] = useState('')
+    const [showField, setShowField] = useState(false)
+    const [showSelectField, setShowSelectField] = useState(true)
 
     const handleChange = (field, values) => {
         setContactData({ ...contactData, [field]: values })
@@ -48,6 +52,8 @@ const ClientDetail = () => {
 
     const handleContactClose = (event) => {
         setShowAddContact(false)
+        setShowField(false)
+        setShowSelectField(true)
     }
 
     const handleContactSubmit = async (event) => {
@@ -75,15 +81,32 @@ const ClientDetail = () => {
         try {
             const res = await axios.get(`http://localhost:8080/api/client/read/${id}`)
             setClient(res.data.data)
-            console.log(client)
+            console.log(contact)
         } catch (error) {
             console.log(error)
         }
     }
 
+    const fetchContacts = async () => {
+        try {
+            const response = await api.get('/contact/read')
+            const data = response.data
+            setContacts(data)
+            console.log(contacts)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     useEffect(() => {
         fetchClient()
+        fetchContacts()
     }, [])
+
+    const handleShowField = () => {
+        setShowField(true)
+        setShowSelectField(false)
+    }
 
     return (
         <div className='py-10 px-20 grid-flow-row'>
@@ -92,34 +115,34 @@ const ClientDetail = () => {
             <div className='flex'>
                 <div className='mr-10'>
                     <div className='py-5'>
-                        <p><span className='font-bold'>ID:</span> {client.ID}</p>
-                        <p><span className='font-bold'>Client Code:</span> {client.client_code || '-'}</p>
-                        <p><span className='font-bold'>Client Name:</span>  {client.client_name || '-'}</p>
-                        <p><span className='font-bold'>Alias:</span> {client.alias || '-'}</p>
-                        <p><span className='font-bold'>Website:</span> {client.website || '-'}</p>
+                        <p><span className='font-bold'>ID:</span> {contact.ID}</p>
+                        <p><span className='font-bold'>Client Code:</span> {contact.client_code || '-'}</p>
+                        <p><span className='font-bold'>Client Name:</span>  {contact.client_name || '-'}</p>
+                        <p><span className='font-bold'>Alias:</span> {contact.alias || '-'}</p>
+                        <p><span className='font-bold'>Website:</span> {contact.website || '-'}</p>
                         {/* <p>Date: {client.date || '-'}</p> */}
                     </div>
 
                     <div className='py-5'>
                         <p className='font-bold text-xl mb-1'>Client Social Presence</p>
-                        {client.client_social_presence && (
+                        {contact.client_social_presence && (
                             <div>
-                                <p><span className='font-bold'>LinkedIn:</span> {client.client_social_presence.linkedin || '-'}</p>
-                                <p><span className='font-bold'>Facebook:</span> {client.client_social_presence.facebook || '-'}</p>
-                                <p><span className='font-bold'>Twitter:</span> {client.client_social_presence.twitter || '-'}</p>
-                                <p><span className='font-bold'>Github:</span> {client.client_social_presence.github || '-'}</p>
-                                <p><span className='font-bold'>Other Social Media:</span> {client.client_social_presence.other?.join(', ') || '-'}</p>
+                                <p><span className='font-bold'>LinkedIn:</span> {contact.client_social_presence.linkedin || '-'}</p>
+                                <p><span className='font-bold'>Facebook:</span> {contact.client_social_presence.facebook || '-'}</p>
+                                <p><span className='font-bold'>Twitter:</span> {contact.client_social_presence.twitter || '-'}</p>
+                                <p><span className='font-bold'>Github:</span> {contact.client_social_presence.github || '-'}</p>
+                                <p><span className='font-bold'>Other Social Media:</span> {contact.client_social_presence.other?.join(', ') || '-'}</p>
                             </div>
                         )}
                     </div>
 
                     <div>
                         <p className='font-bold text-xl mb-1'>Subsidiary</p>
-                        {client.subsidiary && (
+                        {contact.subsidiary && (
                             <div>
-                                <p><span className='font-bold'>Subsidiaries: </span> {client.subsidiary.subsidiaries?.join(', ') || '-'}</p>
-                                <p><span className='font-bold'>Immediate Parents: </span>  {client.subsidiary.immediate_parents?.join(', ') || '-'}</p>
-                                <p><span className='font-bold'>Ultimate Parents: </span>{client.subsidiary.ultimate_parents?.join(', ') || '-'}</p>
+                                <p><span className='font-bold'>Subsidiaries: </span> {contact.subsidiary.subsidiaries?.join(', ') || '-'}</p>
+                                <p><span className='font-bold'>Immediate Parents: </span>  {contact.subsidiary.immediate_parents?.join(', ') || '-'}</p>
+                                <p><span className='font-bold'>Ultimate Parents: </span>{contact.subsidiary.ultimate_parents?.join(', ') || '-'}</p>
                             </div>
                         )}
                     </div>
@@ -139,7 +162,7 @@ const ClientDetail = () => {
                                 </tr>
                             </thead>
                             <tbody>
-                                {client.locations && client.locations.map(location => (
+                                {contact.locations && contact.locations.map(location => (
                                     <tr key={location.ID}>
                                         <td className='border px-4 py-2'>{location.address || '-'}</td>
                                         <td className='border px-4 py-2'>{location.city.city_name || '-'}</td>
@@ -170,23 +193,52 @@ const ClientDetail = () => {
                             Add Contact
                         </h1>
                         <div>
-                            <label htmlFor="name"
-                                className="text-left block text-sm font-medium leading-6 text-gray-900 py-1 pl-1">
-                                Contact Name
-                            </label>
-                            <input
-                                type="text"
-                                autoComplete='off'
-                                className="bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-full block pb-2 p-2.5"
-                                id="name"
-                                value={contactData.contact_name}
-                                onChange={(event) => handleChange('contact_name', event.target.value)}
-                                placeholder='Insert Contact Name' />
-                            {contactError.name && <p className="text-red-500 text-sm text-left pt-1 pl-1">{contactError.name}</p>}
+                            {showSelectField &&
+                                <div>
+                                    <label htmlFor="name"
+                                        className="text-left block text-sm font-medium leading-6 text-gray-900 py-1 pl-1">
+                                        Select Contact
+                                    </label>
+                                    <select id="contacts" className="bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 block w-full p-2.5" value={fetchedContact} onChange={(e) => {
+                                        const value = e.target.value
+                                        if (value === "addContact") {
+                                            handleShowField()
+                                        }
+                                        const selectedContactId = parseInt(e.target.value, 10);
+                                        setFetchedContact(selectedContactId);
+                                    }}>
+                                        <option value="">Select Contact</option>
+                                        <option value="addContact">Add New Contact...</option>
+                                        {Array.isArray(contacts.data) &&
+                                            contacts.data.map((contact) => (
+                                                <option key={contact.ID} value={contact.ID}>
+                                                    {contact.contact_name}
+                                                </option>
+                                            ))}
+                                    </select>
+                                </div>
+                            }
+
+                            {showField &&
+                                <div>
+                                    <label htmlFor="name"
+                                        className="text-left block text-sm font-medium leading-6 text-gray-900 py-1 pl-1">
+                                        Add New Contact
+                                    </label>
+                                    <input
+                                        type="text"
+                                        autoComplete='off'
+                                        className="bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-full block pb-2 p-2.5"
+                                        id="name"
+                                        value={contactData.contact_name}
+                                        onChange={(event) => handleChange('contact_name', event.target.value)}
+                                        placeholder='Insert Contact Name' />
+                                </div>
+                            }
                         </div>
                         <div className="items-center justify-center flex gap-6 mt-4">
-                            <button type="button" onClick={handleContactClose} className="bg-red-700 font-light text-white text-base text-bold py-2 px-4 w-1/2 max-w-full rounded-md hover:bg-red-800 focus:outline-none">Cancel</button>
-                            <button type="button" onClick={handleContactSubmit} className="bg-emerald-700 font-light text-white text-base text-bold py-2 px-4 w-1/2 max-w-full rounded-md hover:bg-emerald-800 focus:outline-none">Submit</button>
+                            <button type="button" onClick={handleContactClose} className="bg-red-700 font-bold text-white text-base text-bold py-2 px-4 w-1/2 max-w-full rounded-md hover:bg-red-800 focus:outline-none">Cancel</button>
+                            <button type="button" onClick={handleContactSubmit} className="bg-emerald-700 font-bold text-white text-base text-bold py-2 px-4 w-1/2 max-w-full rounded-md hover:bg-emerald-800 focus:outline-none">Submit</button>
                         </div>
                     </div>
                     <Snackbar open={!!contactSnackBar} autoHideDuration={2500} onClose={handleSnackbarClose} anchorOrigin={{
