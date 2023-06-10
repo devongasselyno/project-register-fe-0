342e6613b5af0c932ac4f7a24886591b321b8c0a
devongasselyno <devon.gasselyno@soluix.ai>
2023-06-10 18:29:04 +0700

    added client update form

--- src/components/forms/UpdateClient.js
+++ src/components/forms/UpdateClient.js
@@ -13,7 +13,7 @@ const UpdateClient = () => {
             client_name: '',
             alias: '',
             website: '',
-            social_presence: {
+            client_social_presence: {
                 linkedin: '',
                 facebook: '',
                 twitter: '',
@@ -27,20 +27,29 @@ const UpdateClient = () => {
             }
       })
 
-    
-
     const handleChange = (e) => {
-        setFormData((prevData) => ({
-            ...prevData,
-            [e.target.name]: e.target.value,
-        }))
+        const { name, value } = e.target;
+        const updatedFormData = { ...formData }
+    
+        const propertyNames = name.split('.')
+    
+        let currentProperty = updatedFormData;
+        for (let i = 0; i < propertyNames.length - 1; i++) {
+        const propertyName = propertyNames[i]
+        currentProperty = currentProperty[propertyName]
+        }
+        currentProperty[propertyNames[propertyNames.length - 1]] = value
+    
+        setFormData(updatedFormData);
     }
+        
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
+        console.log("formData:", formData)
         try {
-            await axios.put(`http://localhost:8080/api/client/${id}`, formData);
+            await axios.patch(`http://localhost:8080/api/client/${id}`, formData);
             navigate(`/client/read/${id}`);
         } catch (error) {
             console.error('Failed to update client: ', error)
@@ -49,28 +58,31 @@ const UpdateClient = () => {
 
     useEffect(() => {
         const fetchClient = async () => {
-        try {
-                const response = await axios.get(`http://localhost:8080/api/client/read/${id}`)
-                setClient(response.data.data)
-                // console.log("clientt", response.data.data.client_social_presence.linkedin)
-                setFormData({
-                    client_code: response.data.data.client_code || '',
-                    client_name: response.data.data.client_name || '',
-                    alias: response.data.data.alias || '',
-                    website: response.data.data.website || '',
-                    social_presence: {
-                        linkedin: response.data.data.client_social_presence.linkedin || '',
-                        facebook: response.data.data.client_social_presence.facebook || '',
-                        twitter: response.data.data.client_social_presence.twitter || '',
-                        github: response.data.data.client_social_presence.github || '',
-                        other: response.data.data.client_social_presence.other || []
-                    },
-                    subsidiary: {
-                        subsidiaries: response.data.data.subsidiary.subsidiaries || [],
-                        immidiate_parents: response.data.data.subsidiary.immidiate_parents || [],
-                        ultimate_parents: response.data.data.subsidiary.ultimate_parents || []
-                    }
-                })
+            try {
+                const response = await axios.get(`http://localhost:8080/api/client/read/${id}`);
+                const clientData = response.data.data;
+                setClient(clientData);
+        
+                setFormData((prevData) => ({
+                ...prevData,
+                client_code: clientData.client_code || '',
+                client_name: clientData.client_name || '',
+                alias: clientData.alias || '',
+                website: clientData.website || '',
+                client_social_presence: {
+                    linkedin: clientData.client_social_presence.linkedin || '',
+                    facebook: clientData.client_social_presence.facebook || '',
+                    twitter: clientData.client_social_presence.twitter || '',
+                    github: clientData.client_social_presence.github || '',
+                    other: clientData.client_social_presence.other || [],
+                },
+                subsidiary: {
+                    subsidiaries: clientData.subsidiary.subsidiaries || [],
+                    immidiate_parents: clientData.subsidiary.immediate_parents || [],
+                    ultimate_parents: clientData.subsidiary.ultimate_parents || [],
+                },
+                }))
+        
                 setLoading(false);
             } catch (error) {
                 console.error('Failed to fetch client: ', error)
@@ -79,6 +91,7 @@ const UpdateClient = () => {
     
         fetchClient()
     }, [id])
+      
 
     return (
         <div className='py-10 mx-auto max-w-xl flex-col items-center'>
@@ -87,25 +100,25 @@ const UpdateClient = () => {
             <form onSubmit={handleSubmit}>
                 <div className='pb-2'>
                     <label htmlFor="client_name" className='block text-sm font-medium leading-6 text-gray-900 py-1'>Client Name</label>
-                    <input id='client_name' name='client_name' type="text" placeholder={formData.client_name} value={formData.client_name}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
+                    <input id='client_name' name='client_name' type="text" value={formData.client_name}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
                     {errors.client_name && <p className="text-red-500">{errors.client_name}</p>}
                 </div>
 
                 <div className='pb-2'>
                     <label htmlFor="client_code" className='block text-sm font-medium leading-6 text-gray-900 py-1'>Client Code</label>
-                    <input id='client_code' name='client_code' type="text" placeholder={formData.client_code} value={formData.client_code}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
+                    <input id='client_code' name='client_code' type="text" value={formData.client_code}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
                     {errors.client_name && <p className="text-red-500">{errors.client_code}</p>}
                 </div>
 
                 <div className='pb-2'>
                     <label htmlFor="alias" className='block text-sm font-medium leading-6 text-gray-900 py-1'>Alias</label>
-                    <input id='alias' name='alias' type="text" placeholder={formData.alias} value={formData.alias}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
+                    <input id='alias' name='alias' type="text" value={formData.alias}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
                     {errors.alias && <p className="text-red-500">{errors.alias}</p>}
                 </div>
 
                 <div className='pb-2'>
                     <label htmlFor="website" className='block text-sm font-medium leading-6 text-gray-900 py-1'>Website</label>
-                    <input id='website' name='website' type="text" placeholder={formData.website} value={formData.website}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
+                    <input id='website' name='website' type="text" value={formData.website}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
                     {errors.website && <p className="text-red-500">{errors.website}</p>}
                 </div>
 
@@ -113,48 +126,59 @@ const UpdateClient = () => {
                 <h1 className='text-xl font-bold py-5'>Client Social Presence</h1>
                 <div className='pb-2'>
                     <label htmlFor="linkedin" className='block text-sm font-medium leading-6 text-gray-900 py-1'>Linkedin</label>
-                    <input id='linkedin' name='linkedin' type="text" placeholder={formData.social_presence.linkedin} value={formData.social_presence.linkedin}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
+                    <input id='linkedin' name='client_social_presence.linkedin' type="text" value={formData.client_social_presence.linkedin}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
                     {errors.linkedin && <p className="text-red-500">{errors.linkedin}</p>}
                 </div>
 
                 <div className='pb-2'>
-                    <label htmlFor="facebook" className='block text-sm font-medium leading-6 text-gray-900 py-1'>Website</label>
-                    <input id='facebook' name='facebook' type="text" placeholder={formData.social_presence.facebook} value={formData.social_presence.facebook}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
+                    <label htmlFor="facebook" className='block text-sm font-medium leading-6 text-gray-900 py-1'>Facebook</label>
+                    <input id='facebook' name='client_social_presence.facebook' type="text" value={formData.client_social_presence.facebook}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
                     {errors.facebook && <p className="text-red-500">{errors.facebook}</p>}
                 </div>
 
                 <div className='pb-2'>
+                    <label htmlFor="twitter" className='block text-sm font-medium leading-6 text-gray-900 py-1'>Twitter</label>
+                    <input id='twitter' name='client_social_presence.twitter' type="text" value={formData.client_social_presence.twitter}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
+                    {errors.twitter && <p className="text-red-500">{errors.twitter}</p>}
+                </div>
+
+
+                <div className='pb-2'>
                     <label htmlFor="github" className='block text-sm font-medium leading-6 text-gray-900 py-1'>Github</label>
-                    <input id='github' name='github' type="text" placeholder={formData.social_presence.github} value={formData.social_presence.github}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
+                    <input id='github' name='client_social_presence.github' type="text" value={formData.client_social_presence.github}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
                     {errors.github && <p className="text-red-500">{errors.github}</p>}
                 </div>
 
                 <div className='pb-2'>
                     <label htmlFor="other" className='block text-sm font-medium leading-6 text-gray-900 py-1'>Other Social Media</label>
-                    <input id='other' name='other' type="text" placeholder={formData.social_presence.other} value={formData.social_presence.other}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
+                    <input id='other' name='client_social_presence.other' type="text" value={formData.client_social_presence.other}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
                     {errors.other && <p className="text-red-500">{errors.other}</p>}
                 </div>
 
                 <h1 className='text-xl font-bold py-5'>Subsidiary</h1>
                 <div className='pb-2'>
-                    <label htmlFor="subsidiaries" className='block text-sm font-medium leading-6 text-gray-900 py-1'>Other Social Media</label>
-                    <input id='github' name='github' type="text" placeholder={formData.subsidiary.subsidiaries} value={formData.subsidiary.subsidiaries}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
+                    <label htmlFor="subsidiaries" className='block text-sm font-medium leading-6 text-gray-900 py-1'>Subsidiaries</label>
+                    <input id='subsidiaries' name='subsidiary.subsidiaries' type="text" value={formData.subsidiary.subsidiaries}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
                     {errors.subsidiaries && <p className="text-red-500">{errors.subsidiaries}</p>}
                 </div>
 
                 <div className='pb-2'>
                     <label htmlFor="immidiate_parents" className='block text-sm font-medium leading-6 text-gray-900 py-1'>Immediate Parents</label>
-                    <input id='github' name='github' type="text" placeholder={formData.subsidiary.immidiate_parents} value={formData.subsidiary.immidiate_parents}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
+                    <input id='immidiate_parents' name='subsidiary.immidiate_parents' type="text" value={formData.subsidiary.immidiate_parents}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
                     {errors.immidiate_parents && <p className="text-red-500">{errors.immidiate_parents}</p>}
                 </div>
 
                 <div className='pb-2'>
                     <label htmlFor="ultimate_parents" className='block text-sm font-medium leading-6 text-gray-900 py-1'>Ultimate Parents</label>
-                    <input id='github' name='github' type="text" placeholder={formData.subsidiary.ultimate_parents} value={formData.subsidiary.ultimate_parents}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
+                    <input id='ultimate_parents' name='subsidiary.ultimate_parents' type="text" value={formData.subsidiary.ultimate_parents}  onChange={handleChange} className='w-full bg-gray-100 border border-zinc-400 text-gray-900 text-sm rounded focus:ring-orange-700 focus:border-orange-700 w-1/5'/>
                     {errors.ultimate_parents && <p className="text-red-500">{errors.ultimate_parents}</p>}
                 </div>
 
-                <button type='submit' className='float-right my-5 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded'>Submit</button>
+                <div className='float-right'>
+                    <button onClick={() => navigate(`/client/read/${id}`)} className='my-5 mr-5 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>Cancel</button>
+                    <button type='submit' className=' my-5 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded'>Submit</button>
+                </div>
+
             </form>
         </div>
     )
